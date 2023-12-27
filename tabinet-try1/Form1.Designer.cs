32506cffe2550d7ab6234bdcf4b3415db9b4b9a5
anean <anean@MATA>
2023-12-27 18:11:44 +0200

    Add project files.

---
+++ tabinet-try1/Form1.Designer.cs
@@ -0,0 +1,343 @@
+﻿namespace tabinet_try1
+{
+    partial class Tabla
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Tabla));
+            this.p1_card4 = new System.Windows.Forms.PictureBox();
+            this.p1_card5 = new System.Windows.Forms.PictureBox();
+            this.p1_card2 = new System.Windows.Forms.PictureBox();
+            this.p1_card3 = new System.Windows.Forms.PictureBox();
+            this.p1_card1 = new System.Windows.Forms.PictureBox();
+            this.p1_card6 = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.picturedeck = new System.Windows.Forms.PictureBox();
+            this.p2_card4 = new System.Windows.Forms.PictureBox();
+            this.p2_card5 = new System.Windows.Forms.PictureBox();
+            this.p2_card3 = new System.Windows.Forms.PictureBox();
+            this.p2_card2 = new System.Windows.Forms.PictureBox();
+            this.p2_card1 = new System.Windows.Forms.PictureBox();
+            this.p2_card6 = new System.Windows.Forms.PictureBox();
+            this.tabla_card1 = new System.Windows.Forms.PictureBox();
+            this.tabla_card2 = new System.Windows.Forms.PictureBox();
+            this.tabla_card3 = new System.Windows.Forms.PictureBox();
+            this.tabla_card4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.p1_card4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1_card5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1_card2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1_card3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1_card1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1_card6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picturedeck)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2_card4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2_card5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2_card3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2_card2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2_card1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2_card6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tabla_card1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tabla_card2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tabla_card3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tabla_card4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // p1_card4
+            // 
+            this.p1_card4.BackColor = System.Drawing.Color.Transparent;
+            this.p1_card4.Location = new System.Drawing.Point(362, 346);
+            this.p1_card4.Name = "p1_card4";
+            this.p1_card4.Size = new System.Drawing.Size(59, 82);
+            this.p1_card4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p1_card4.TabIndex = 0;
+            this.p1_card4.TabStop = false;
+            this.p1_card4.Visible = false;
+            // 
+            // p1_card5
+            // 
+            this.p1_card5.BackColor = System.Drawing.Color.Transparent;
+            this.p1_card5.Location = new System.Drawing.Point(427, 346);
+            this.p1_card5.Name = "p1_card5";
+            this.p1_card5.Size = new System.Drawing.Size(59, 82);
+            this.p1_card5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p1_card5.TabIndex = 1;
+            this.p1_card5.TabStop = false;
+            this.p1_card5.Visible = false;
+            // 
+            // p1_card2
+            // 
+            this.p1_card2.BackColor = System.Drawing.Color.Transparent;
+            this.p1_card2.Location = new System.Drawing.Point(229, 346);
+            this.p1_card2.Name = "p1_card2";
+            this.p1_card2.Size = new System.Drawing.Size(59, 82);
+            this.p1_card2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p1_card2.TabIndex = 2;
+            this.p1_card2.TabStop = false;
+            this.p1_card2.Visible = false;
+            // 
+            // p1_card3
+            // 
+            this.p1_card3.BackColor = System.Drawing.Color.Transparent;
+            this.p1_card3.Location = new System.Drawing.Point(296, 346);
+            this.p1_card3.Name = "p1_card3";
+            this.p1_card3.Size = new System.Drawing.Size(59, 82);
+            this.p1_card3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p1_card3.TabIndex = 3;
+            this.p1_card3.TabStop = false;
+            this.p1_card3.Visible = false;
+            // 
+            // p1_card1
+            // 
+            this.p1_card1.BackColor = System.Drawing.Color.Transparent;
+            this.p1_card1.Location = new System.Drawing.Point(163, 346);
+            this.p1_card1.Name = "p1_card1";
+            this.p1_card1.Size = new System.Drawing.Size(60, 82);
+            this.p1_card1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p1_card1.TabIndex = 4;
+            this.p1_card1.TabStop = false;
+            this.p1_card1.Visible = false;
+            this.p1_card1.Click += new System.EventHandler(this.p1_card1_Click);
+            // 
+            // p1_card6
+            // 
+            this.p1_card6.BackColor = System.Drawing.Color.Transparent;
+            this.p1_card6.Location = new System.Drawing.Point(493, 346);
+            this.p1_card6.Name = "p1_card6";
+            this.p1_card6.Size = new System.Drawing.Size(60, 82);
+            this.p1_card6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p1_card6.TabIndex = 5;
+            this.p1_card6.TabStop = false;
+            this.p1_card6.Visible = false;
+            this.p1_card6.Click += new System.EventHandler(this.p1_card6_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.Transparent;
+            this.btnStart.Location = new System.Drawing.Point(339, 182);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(101, 53);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.Text = "Incepe un joc nou";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // picturedeck
+            // 
+            this.picturedeck.BackColor = System.Drawing.Color.Transparent;
+            this.picturedeck.Image = ((System.Drawing.Image)(resources.GetObject("picturedeck.Image")));
+            this.picturedeck.Location = new System.Drawing.Point(26, 164);
+            this.picturedeck.Name = "picturedeck";
+            this.picturedeck.Size = new System.Drawing.Size(60, 92);
+            this.picturedeck.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picturedeck.TabIndex = 7;
+            this.picturedeck.TabStop = false;
+            this.picturedeck.Visible = false;
+            this.picturedeck.Click += new System.EventHandler(this.picturedeck_Click);
+            // 
+            // p2_card4
+            // 
+            this.p2_card4.BackColor = System.Drawing.Color.Transparent;
+            this.p2_card4.Location = new System.Drawing.Point(361, 12);
+            this.p2_card4.Name = "p2_card4";
+            this.p2_card4.Size = new System.Drawing.Size(60, 82);
+            this.p2_card4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p2_card4.TabIndex = 8;
+            this.p2_card4.TabStop = false;
+            this.p2_card4.Visible = false;
+            // 
+            // p2_card5
+            // 
+            this.p2_card5.BackColor = System.Drawing.Color.Transparent;
+            this.p2_card5.Location = new System.Drawing.Point(427, 12);
+            this.p2_card5.Name = "p2_card5";
+            this.p2_card5.Size = new System.Drawing.Size(60, 82);
+            this.p2_card5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p2_card5.TabIndex = 9;
+            this.p2_card5.TabStop = false;
+            this.p2_card5.Visible = false;
+            // 
+            // p2_card3
+            // 
+            this.p2_card3.BackColor = System.Drawing.Color.Transparent;
+            this.p2_card3.Location = new System.Drawing.Point(295, 12);
+            this.p2_card3.Name = "p2_card3";
+            this.p2_card3.Size = new System.Drawing.Size(60, 82);
+            this.p2_card3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p2_card3.TabIndex = 10;
+            this.p2_card3.TabStop = false;
+            this.p2_card3.Visible = false;
+            // 
+            // p2_card2
+            // 
+            this.p2_card2.BackColor = System.Drawing.Color.Transparent;
+            this.p2_card2.Location = new System.Drawing.Point(229, 12);
+            this.p2_card2.Name = "p2_card2";
+            this.p2_card2.Size = new System.Drawing.Size(60, 82);
+            this.p2_card2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p2_card2.TabIndex = 11;
+            this.p2_card2.TabStop = false;
+            this.p2_card2.Visible = false;
+            // 
+            // p2_card1
+            // 
+            this.p2_card1.BackColor = System.Drawing.Color.Transparent;
+            this.p2_card1.Location = new System.Drawing.Point(163, 12);
+            this.p2_card1.Name = "p2_card1";
+            this.p2_card1.Size = new System.Drawing.Size(60, 82);
+            this.p2_card1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p2_card1.TabIndex = 12;
+            this.p2_card1.TabStop = false;
+            this.p2_card1.Visible = false;
+            // 
+            // p2_card6
+            // 
+            this.p2_card6.BackColor = System.Drawing.Color.Transparent;
+            this.p2_card6.Location = new System.Drawing.Point(493, 12);
+            this.p2_card6.Name = "p2_card6";
+            this.p2_card6.Size = new System.Drawing.Size(60, 82);
+            this.p2_card6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p2_card6.TabIndex = 13;
+            this.p2_card6.TabStop = false;
+            this.p2_card6.Visible = false;
+            // 
+            // tabla_card1
+            // 
+            this.tabla_card1.BackColor = System.Drawing.Color.Transparent;
+            this.tabla_card1.Location = new System.Drawing.Point(202, 164);
+            this.tabla_card1.Name = "tabla_card1";
+            this.tabla_card1.Size = new System.Drawing.Size(60, 82);
+            this.tabla_card1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.tabla_card1.TabIndex = 14;
+            this.tabla_card1.TabStop = false;
+            this.tabla_card1.Visible = false;
+            // 
+            // tabla_card2
+            // 
+            this.tabla_card2.BackColor = System.Drawing.Color.Transparent;
+            this.tabla_card2.Location = new System.Drawing.Point(284, 164);
+            this.tabla_card2.Name = "tabla_card2";
+            this.tabla_card2.Size = new System.Drawing.Size(60, 82);
+            this.tabla_card2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.tabla_card2.TabIndex = 15;
+            this.tabla_card2.TabStop = false;
+            this.tabla_card2.Visible = false;
+            // 
+            // tabla_card3
+            // 
+            this.tabla_card3.BackColor = System.Drawing.Color.Transparent;
+            this.tabla_card3.Location = new System.Drawing.Point(362, 164);
+            this.tabla_card3.Name = "tabla_card3";
+            this.tabla_card3.Size = new System.Drawing.Size(60, 82);
+            this.tabla_card3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.tabla_card3.TabIndex = 16;
+            this.tabla_card3.TabStop = false;
+            this.tabla_card3.Visible = false;
+            // 
+            // tabla_card4
+            // 
+            this.tabla_card4.BackColor = System.Drawing.Color.Transparent;
+            this.tabla_card4.Location = new System.Drawing.Point(446, 164);
+            this.tabla_card4.Name = "tabla_card4";
+            this.tabla_card4.Size = new System.Drawing.Size(60, 82);
+            this.tabla_card4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.tabla_card4.TabIndex = 17;
+            this.tabla_card4.TabStop = false;
+            this.tabla_card4.Visible = false;
+            // 
+            // Tabla
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabla_card4);
+            this.Controls.Add(this.tabla_card3);
+            this.Controls.Add(this.tabla_card2);
+            this.Controls.Add(this.tabla_card1);
+            this.Controls.Add(this.p2_card6);
+            this.Controls.Add(this.p2_card1);
+            this.Controls.Add(this.p2_card2);
+            this.Controls.Add(this.p2_card3);
+            this.Controls.Add(this.p2_card5);
+            this.Controls.Add(this.p2_card4);
+            this.Controls.Add(this.picturedeck);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.p1_card6);
+            this.Controls.Add(this.p1_card1);
+            this.Controls.Add(this.p1_card3);
+            this.Controls.Add(this.p1_card2);
+            this.Controls.Add(this.p1_card5);
+            this.Controls.Add(this.p1_card4);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Tabla";
+            this.Text = "Tabla de joc";
+            this.Load += new System.EventHandler(this.Tabla_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.p1_card4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1_card5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1_card2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1_card3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1_card1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1_card6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picturedeck)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2_card4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2_card5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2_card3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2_card2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2_card1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2_card6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tabla_card1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tabla_card2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tabla_card3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tabla_card4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox p1_card4;
+        private System.Windows.Forms.PictureBox p1_card5;
+        private System.Windows.Forms.PictureBox p1_card2;
+        private System.Windows.Forms.PictureBox p1_card3;
+        private System.Windows.Forms.PictureBox p1_card1;
+        private System.Windows.Forms.PictureBox p1_card6;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.PictureBox picturedeck;
+        private System.Windows.Forms.PictureBox p2_card4;
+        private System.Windows.Forms.PictureBox p2_card5;
+        private System.Windows.Forms.PictureBox p2_card3;
+        private System.Windows.Forms.PictureBox p2_card2;
+        private System.Windows.Forms.PictureBox p2_card1;
+        private System.Windows.Forms.PictureBox p2_card6;
+        private System.Windows.Forms.PictureBox tabla_card1;
+        private System.Windows.Forms.PictureBox tabla_card2;
+        private System.Windows.Forms.PictureBox tabla_card3;
+        private System.Windows.Forms.PictureBox tabla_card4;
+    }
+}
+
---
+++ tabinet-try1/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using tabinet_try1.Classes;
+
+namespace tabinet_try1
+{
+    public partial class Tabla : Form
+    {
+        Deck deck = new Deck();
+        Player player1 = new Player("Player 1");
+        Player player2 = new Player("Player 2");
+
+        Board board = new Board();
+
+        public void Initial()
+        {
+            deck.Shuffle();
+            for (int i = 0; i < 6; i++)
+            {
+                Card cardP1 = deck.DrawCard();
+                player1.AddCardToHand(cardP1);
+                Card cardP2 = deck.DrawCard();
+                player2.AddCardToHand(cardP2);
+            }
+            for(int i = 0; i < 4; i ++)
+            {
+                Card cardboard = deck.DrawCard();
+                board.AddCardToBoard(cardboard);
+            }
+        }
+
+        public string ImageString(Card card)
+        {
+            return card.Rank + "_of_" + card.Suit + ".png";
+        }
+        
+        public void UpdateHandP1()
+        {
+            p1_card1.Image = Image.FromFile("../../Images/" + ImageString(player1.Hand[0]));
+            p1_card2.Image = Image.FromFile("../../Images/" + ImageString(player1.Hand[1]));
+            p1_card3.Image = Image.FromFile("../../Images/" + ImageString(player1.Hand[2]));
+            p1_card4.Image = Image.FromFile("../../Images/" + ImageString(player1.Hand[3]));
+            p1_card5.Image = Image.FromFile("../../Images/" + ImageString(player1.Hand[4]));
+            p1_card6.Image = Image.FromFile("../../Images/" + ImageString(player1.Hand[5]));
+        }
+        public void UpdateHandP2()
+        {
+            p2_card1.Image = Image.FromFile("../../Images/" + ImageString(player2.Hand[0]));
+            p2_card2.Image = Image.FromFile("../../Images/" + ImageString(player2.Hand[1]));
+            p2_card3.Image = Image.FromFile("../../Images/" + ImageString(player2.Hand[2]));
+            p2_card4.Image = Image.FromFile("../../Images/" + ImageString(player2.Hand[3]));
+            p2_card5.Image = Image.FromFile("../../Images/" + ImageString(player2.Hand[4]));
+            p2_card6.Image = Image.FromFile("../../Images/" + ImageString(player2.Hand[5]));
+        }
+
+        public void UpdateBoard()
+        {
+            tabla_card1.Image = Image.FromFile("../../Images/" + ImageString(board.Tabla[0]));
+            tabla_card2.Image = Image.FromFile("../../Images/" + ImageString(board.Tabla[1]));
+            tabla_card3.Image = Image.FromFile("../../Images/" + ImageString(board.Tabla[2]));
+            tabla_card4.Image = Image.FromFile("../../Images/" + ImageString(board.Tabla[3]));
+        }
+        public Tabla()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Tabla_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void p1_card1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Initial();
+            UpdateHandP1();
+            UpdateHandP2();
+            UpdateBoard();
+            btnStart.Hide();
+            picturedeck.Show();
+            
+
+        }
+        private int ct = 0;
+        private void picturedeck_Click(object sender, EventArgs e)
+        {
+
+            if (ct % 3 == 0)
+            {
+                p1_card1.Show(); 
+                p1_card2.Show();
+                p1_card3.Show(); 
+                p1_card4.Show(); 
+                p1_card5.Show(); 
+                p1_card6.Show();
+                ct++;
+            }
+
+            else if (ct % 3 == 1)
+            {
+                p2_card1.Show();
+                p2_card2.Show(); 
+                p2_card3.Show(); 
+                p2_card4.Show(); 
+                p2_card5.Show(); 
+                p2_card6.Show();
+                ct++;
+            }
+
+            else if(ct % 3 == 2)
+            {
+                tabla_card1.Show();
+                tabla_card2.Show();
+                tabla_card3.Show();
+                tabla_card4.Show();
+                ct++;
+            }
+        }
+
+        private void p1_card6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
